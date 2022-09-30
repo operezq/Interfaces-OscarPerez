e86ecf00add07ee0b85c783087b48cb16e6d30df
Oscar Pérez <111613509+operezq@users.noreply.github.com>
2022-09-29 21:41:17 -0500

    Add files via upload

---
+++ InterfazEj22/src/main/java/poo/interfazej22/InterfazEj22.java
@@ -0,0 +1,13 @@
+/*Elaborar un algoritmo que entre el nombre de un empleado, su salario básico por hora y el 
+número de horas trabajadas en el mes; escriba su nombre y salario mensual si éste es mayor 
+de $450.000, de lo contrario escriba sólo el nombre. */
+
+package poo.interfazej22;
+public class InterfazEj22 {
+
+    public static void main(String[] args) {
+        Form22 fr = new Form22();
+        fr.setVisible(true);
+        
+    }
+}
